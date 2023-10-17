3626c9c42e0271e8f045518b586263b343c77c58
tofunmiadewuyi <40152992+tofunmiadewuyi@users.noreply.github.com>
2023-10-17 18:52:28 +0000

    Event listeners
    Added new event listeners to control the behaviour of the options menu, the task when clicked, the input button and more.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const taskInput = document.querySelector(".new-task input");
+document.title = "Tofunmi's To-do";
 
 let taskList = document.getElementById("task-list");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-function createItem(todo) {
+function createItem(todo, isCompleted) {
   //create the item
   const todoEl = document.createElement("div");
   todoEl.classList.add("task");
 
   todoEl.innerHTML = `
   <label for="${todo.id}">
-    <input type="checkbox" id="${todo.id}">
-    <p>${todo.name}</p>
+    <input onclick="updateStatus(this)" type="checkbox" id="${todo.id}" ${isCompleted}>
+    <p class="${isCompleted}">${todo.name}</p>
   </label>
   `
   taskList.appendChild(todoEl);
@@ -59,6 +59,7 @@ function createItem(todo) {
   optionsMenu.classList.add("task-options");
 
   const optionsIcon = document.createElement("img");
+  optionsIcon.classList.add("options-icon");
   optionsIcon.src = "svgs/options1.svg";
 
   //append menu icon + menu tray to the menu button
@@ -68,15 +69,6 @@ function createItem(todo) {
   //append the menu to the task
   todoEl.appendChild(optionsMenu);
 
-  //add event listener to the menu button
-  optionsMenu.addEventListener("click", () => {
-    if (menu.style.display === "none") {
-      menu.style.display = "flex";
-
-    } else {
-      menu.style.display = "none";
-    }
-  })
 
   //add event listener to the delete option
   deleteOption.addEventListener("click", () => {
@@ -92,24 +84,106 @@ function createItem(todo) {
 
 function getTodos() {
   todos.forEach((todo, index) => {
-    createItem(todo);
+    let isCompleted = todo.status == "completed" ? "checked" : "";
+    createItem(todo, isCompleted);
   })
 }
 
 getTodos();
 
+
+function getLatestItem() {
+  const latestItem = todos[todos.length - 1];
+  createItem(latestItem);
+}
+
+function updateStatus(selectedTask) {
+  let taskName = selectedTask.parentNode.querySelector("p");
+  //get task index
+  const idToCheck = parseInt(selectedTask.id, 10);
+
+  const indexToCheck = todos.findIndex(todo => todo.id === idToCheck);
+  //check task status
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    console.log(idToCheck);
+    console.log(indexToCheck);
+    todos[indexToCheck].status = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[indexToCheck].status = "pending";
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//add event listener to the task divs
+const taskItem = document.querySelectorAll(".task");
+
+// taskItem.forEach(task => {
+//   task.addEventListener("click", () => {
+//     const taskInputEl = task.querySelector("input");
+//     taskInputEl.click();
+//   })
+
+// })
+
+//add event listener to the submit button 
+const inputSubmitBtn = document.getElementById("submit");
+inputSubmitBtn.addEventListener("click", () => {
+  addTodo();
+})
+
+//add event listener to the enter key
+const taskInput = document.querySelector(".new-task input");
 taskInput.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
-    const task = taskInput.value.trim();
+    addTodo();
+    
+  }
+});
+
+function addTodo() {
+  const task = taskInput.value.trim();
+  if (task != "") {
     let taskInfo = {name: task, id: Date.now(), status: "pending" };
     todos.push(taskInfo);
     localStorage.setItem("todos", JSON.stringify(todos));
     taskInput.value = "";
     getLatestItem();
   }
-});
-
-function getLatestItem() {
-  const latestItem = todos[todos.length - 1];
-  createItem(latestItem);
 }
+
+// Add a click event listener to the document
+document.addEventListener('click', function(event) {
+
+    function closeMenus() {
+      const menus = document.querySelectorAll('.options-menu');
+      menus.forEach(function(menu) {
+      menu.style.display = 'none';
+          });
+    }
+
+    // Check if the click target is inside the options menu
+    if (event.target.matches('.task-options') || event.target.matches(".options-icon")) {
+      
+      const task = event.target.closest(".task");
+      const optionsMenu = task.querySelector('.options-menu');
+      if (optionsMenu.style.display == "flex") {
+        optionsMenu.style.display = "none";
+      } else {
+        closeMenus();
+        optionsMenu.style.display = "flex"; 
+      }
+      
+    } else if (event.target.matches('.task')) {
+       const task = event.target.closest(".task");
+       const taskInputEl = task.querySelector("input");
+       taskInputEl.click();
+      
+    } else {
+      // Close all option menus by hiding them
+      closeMenus();
+    }
+
+  
+});
